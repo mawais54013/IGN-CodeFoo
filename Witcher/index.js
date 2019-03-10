57690e0e3302f938b056d9817eff663c078fe669
MAwais <mawais54013@outlook.com>
2019-03-09 21:20:28 -0800

    witcher project

---
+++ Witcher/index.js
@@ -0,0 +1,165 @@
+let helmets = [
+    {
+        name: 'Serpentine Cruz Headpiece ',
+        price: 90,
+        value: 23,
+    },
+    {
+        name: 'Keeton Mask',
+        price: 77,
+        value: 24,
+    },
+    {
+        name: 'Feline Visor',
+        price: 68,
+        value: 16,
+    },
+    {
+        name: 'Ornate Helmet of Cagampan',
+        price: 60,
+        value: 16,
+    },
+    {
+        name: 'Offner Protector',
+        price: 54,
+        value: 15,
+    },
+    {
+        name: 'Leather Helmet',
+        price: 49,
+        value: 13,
+    },
+    {
+        name: 'Glass Bowl',
+        price: 44,
+        value: 12,
+    }
+];
+
+let leggings = [
+    {
+        name: 'Famed Pon Leggings',
+        price: 87,
+        value: 23,
+    },
+    {
+        name: 'Ursine Trousers',
+        price: 78,
+        value: 12,
+    },
+    {
+        name: 'Wolven Shinguards',
+        price: 75,
+        value: 15,
+    },
+    {
+        name: 'Hansen Breeches',
+        price: 69,
+        value: 17,
+    },
+    {
+        name: 'Griffin Pants',
+        price: 62,
+        value: 11,
+    },
+    {
+        name: 'Tanned Leg Protection',
+        price: 59,
+        value: 15,
+    },
+    {
+        name: 'Manticore Braces',
+        price: 56,
+        value: 12,
+    },
+    {
+        name: 'Mail Emares Leggings',
+        price: 53,
+        value: 14,
+    },
+    {
+        name: 'Woven Leggings',
+        price: 47,
+        value: 11,
+    },
+    {
+        name: 'Silken Pants',
+        price: 45,
+        value: 10,
+    },
+    {
+        name: 'Tattered Shorts',
+        price: 42,
+        value: 13,
+    },
+];
+
+let chests = [
+    {
+        name: 'Armor de Jandro',
+        price: 67,
+        value: 22,
+    },
+    {
+        name: 'Chestpiece of Vachon',
+        price: 64,
+        value: 23,
+    },
+    {
+        name: 'Kaer Morhen Armor',
+        price: 62,
+        value: 21,
+    },
+    {
+        name: 'Cured Leather Chestpiece',
+        price: 59,
+        value: 20,
+    },
+    {
+        name: 'Smiths Plated Chestguard',
+        price: 58,
+        value: 10,
+    },
+    {
+        name: 'Dented Plate Armor',
+        price: 57,
+        value: 19,
+    },
+    {
+        name: 'Jeweled Drake Tunic',
+        price: 55,
+        value: 19,
+    },
+    {
+        name: 'Gingers Glided Armor',
+        price: 54,
+        value: 18,
+    },
+    {
+        name: 'Garcia Guard',
+        price: 50,
+        value: 17,
+    },
+]
+
+let coins = 300;
+let max = 0;
+let setArmor = [];
+
+function findArmor(money, highNum)
+{
+    let helmet = '';
+    let legging = '';
+    let chest = '';
+
+    let hNum = 0;
+    let lNum = 0;
+    let cNum = 0;
+
+    let total = 0;
+
+    for(let i = 0; i < helmets.length; i++)
+    {
+        
+    }
+}
