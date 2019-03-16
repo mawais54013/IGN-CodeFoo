55779f7e773eafbc7873f1715eb7524afce4f2cc
MAwais <mawais54013@outlook.com>
2019-03-15 21:14:12 -0700

    added

--- Front-End/app.js
+++ Front-End/app.js
@@ -176,3 +176,10 @@ function GetAge(publishDate){
         return days+"d"
     }
 };
+
+function time_convert(num)
+ { 
+  var hours = Math.floor(num / 60);  
+  var minutes = num % 60;
+  return hours + ":" + minutes;         
+}
